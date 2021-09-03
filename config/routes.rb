856a0d0c5ea9535947a0b0c510b8523bafb87f6d
Rafael Briones <rbrionesdev@outlook.com>
2021-09-02 23:48:20 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
-  # root "books#app"
+  root "books#app"
   
-  # resources :books
+  resources :books
   
   # # get '/books', to: 'books#index'
   # # get '/items/new', to:'items#new' # we don't need this
   # # post '/books', to:'books#create'
     # resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'books#app'
+  # root 'books#app'
 
-  get '/books', to: 'books#index'
-  # get '/books/new', to:'books#new' # we don't need this
-  post '/books', to:'books#create'
+  # get '/books', to: 'books#index'
+  # # get '/books/new', to:'books#new' # we don't need this
+  # post '/books', to:'books#create'
 
-  put '/books/:id', to:'books#update'
-  patch '/books/:id', to:'books#update'
+  # put '/books/:id', to:'books#update'
+  # patch '/books/:id', to:'books#update'
 
-  put 'books/:id/like', to: 'books#like'
+  # put 'books/:id/like', to: 'books#like'
 
-  delete '/books/:id', to:'books#destroy'
+  # delete '/books/:id', to:'books#destroy'
 end
